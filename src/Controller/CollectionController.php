d4f106b09717fde8e20fdf9eabcd1b68be5e78d6
stephenlege <sl.professional.email@gmail.com>
2025-04-02 09:39:53 -0500

    added excel import feature correctly

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -24,7 +24,7 @@ use Doctrine\Common\Collections\Criteria;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
